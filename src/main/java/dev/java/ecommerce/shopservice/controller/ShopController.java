6dbb067a805bd55be59bb674ae3a9d61d505cfca
Lucca Nascimento <luccadev@hotmail.com>
2026-03-11 16:21:33 -0300

    feat(api): desenvolve operacoes completas de crud e endpoint de pagamento para o carrinho

--- src/main/java/dev/java/ecommerce/shopservice/controller/ShopController.java
+++ src/main/java/dev/java/ecommerce/shopservice/controller/ShopController.java
@@ -1,5 +1,6 @@
 package dev.java.ecommerce.shopservice.controller;
 
+import dev.java.ecommerce.shopservice.controller.request.PaymentRequest;
 import dev.java.ecommerce.shopservice.controller.request.ShopRequest;
 import dev.java.ecommerce.shopservice.entities.Shop;
 import dev.java.ecommerce.shopservice.service.ShopService;
@@ -23,5 +24,21 @@ public class ShopController {
     @PostMapping
     public ResponseEntity<Shop> createShop(@RequestBody ShopRequest request){
         return ResponseEntity.status(HttpStatus.CREATED).body(shopService.createShop(request));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Shop> updateShop(@PathVariable String id, @RequestBody ShopRequest request){
+        return ResponseEntity.status(HttpStatus.OK).body(shopService.updateShop(id, request));
+    }
+
+    @PutMapping("/{id}/payment")
+    public ResponseEntity<Shop> payShop(@PathVariable String id, @RequestBody PaymentRequest request){
+        return ResponseEntity.status(HttpStatus.OK).body(shopService.payShop(id, request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteShop(@PathVariable String id){
+        shopService.deleteShop(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/dev/java/ecommerce/shopservice/service/ShopService.java
+++ src/main/java/dev/java/ecommerce/shopservice/service/ShopService.java
@@ -1,18 +1,21 @@
 package dev.java.ecommerce.shopservice.service;
 
 import dev.java.ecommerce.shopservice.client.response.PlatziProductResponse;
+import dev.java.ecommerce.shopservice.controller.request.PaymentRequest;
 import dev.java.ecommerce.shopservice.controller.request.ShopRequest;
 import dev.java.ecommerce.shopservice.entities.Product;
 import dev.java.ecommerce.shopservice.entities.Shop;
 import dev.java.ecommerce.shopservice.entities.Status;
+import dev.java.ecommerce.shopservice.exceptions.BusinessException;
+import dev.java.ecommerce.shopservice.exceptions.DataNotFoundException;
 import dev.java.ecommerce.shopservice.repository.ShopRepository;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
+import org.jspecify.annotations.NonNull;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -22,29 +25,18 @@ public class ShopService {
     private final ShopRepository shopRepository;
     private final ProductService productService;
 
-    public Shop getShopById(String id){
+    public Shop getShopById(String id) {
         return shopRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("Shop not Found"));
+                .orElseThrow(() -> new DataNotFoundException("Shop not Found"));
     }
 
-    public Shop createShop(ShopRequest request){
-
+    public Shop createShop(ShopRequest request) {
         shopRepository.findByClientAndStatus(request.clientId(), Status.OPEN)
                 .ifPresent(shop -> {
-                    throw new IllegalArgumentException("There is already an open basket for this client");
+                    throw new BusinessException("There is already an open shop for this client");
                 });
 
-        List<Product> products = new ArrayList<>();
-        request.products().forEach(productRequest -> {
-            PlatziProductResponse platziProductResponse = productService.getProductsById(productRequest.id());
-            products.add(Product.builder()
-                    .id(platziProductResponse.id())
-                    .title(platziProductResponse.title())
-                    .price(platziProductResponse.price())
-                    .quantity(productRequest.quantity())
-                    .build());
-
-        });
+        List<Product> products = getProducts(request);
 
 
         Shop shop = Shop.builder()
@@ -55,5 +47,42 @@ public class ShopService {
 
         shop.calculateTotalPrice();
         return shopRepository.save(shop);
+    }
+
+    private @NonNull List<Product> getProducts(ShopRequest request) {
+        List<Product> products = new ArrayList<>();
+        return request.products().stream().map(productRequest -> {
+                    PlatziProductResponse platziProductResponse = productService.getProductsById(productRequest.id());
+                    return Product.builder()
+                            .id(platziProductResponse.id())
+                            .title(platziProductResponse.title())
+                            .price(platziProductResponse.price())
+                            .quantity(productRequest.quantity())
+                            .build();
+                })
+                .toList();
+    }
+
+    public Shop updateShop(String shopId, ShopRequest request) {
+        Shop savedShop = getShopById(shopId);
+
+        List<Product> products = getProducts(request);
+
+        savedShop.setProducts(products);
+
+        savedShop.calculateTotalPrice();
+        return shopRepository.save(savedShop);
+
+    }
+
+    public Shop payShop(String shopId, PaymentRequest request) {
+        Shop savedShop = getShopById(shopId);
+        savedShop.setPaymentMethod(request.getPaymentMethod());
+        savedShop.setStatus(Status.SOLD);
+        return shopRepository.save(savedShop);
+    }
+
+    public void deleteShop(String id) {
+        shopRepository.delete(getShopById(id));
     }
 }
