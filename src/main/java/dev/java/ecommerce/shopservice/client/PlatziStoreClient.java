5bd5165f7d6fda0123a7dc577d85b8f72fb60bf2
Lucca Nascimento <luccadev@hotmail.com>
2026-03-11 09:23:10 -0300

    feat(client): integra feign client para consumo da api da platzi store

---
+++ src/main/java/dev/java/ecommerce/shopservice/client/PlatziStoreClient.java
@@ -0,0 +1,19 @@
+package dev.java.ecommerce.shopservice.client;
+
+import dev.java.ecommerce.shopservice.client.response.PlatziProductResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+@Repository
+@FeignClient(name = "PlatziStoreClient", url = "${shop.client.platzi}")
+public interface PlatziStoreClient {
+
+    @GetMapping("/products")
+    List<PlatziProductResponse> getAllProducts();
+
+    @GetMapping("/products/{id}")
+    PlatziProductResponse getProductsById(@PathVariable Long id);
+}
