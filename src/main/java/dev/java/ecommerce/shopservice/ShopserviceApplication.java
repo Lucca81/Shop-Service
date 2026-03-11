62afd9fabf6b76e5f7e9b0fcfc867a0c32ab0e8d
Lucca Nascimento <luccadev@hotmail.com>
2026-03-11 09:23:01 -0300

    chore(infra): setup inicial, docker e configuracoes do spring boot

--- src/main/java/dev/java/ecommerce/shopservice/ShopserviceApplication.java
+++ src/main/java/dev/java/ecommerce/shopservice/ShopserviceApplication.java
@@ -2,8 +2,13 @@ package dev.java.ecommerce.shopservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
+@EnableCaching
 @SpringBootApplication
+
 public class ShopserviceApplication {
 
 	public static void main(String[] args) {
